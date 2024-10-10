fa1834c7883ca42d1d353a47e4265370dab074a1
sardor-kushakov <sardorkushakov.uz@gmail.com>
2024-10-10 15:19:18 +0500

    Updated comments and documentation for clarity in the file upload and download project

--- src/main/java/sarik/dev/fileuploaddownload/FileUploadDownloadApplication.java
+++ src/main/java/sarik/dev/fileuploaddownload/FileUploadDownloadApplication.java
@@ -7,16 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * FileUploadDownloadApplication - Spring Boot ilovasi uchun asosiy klass.
  * Ushbu klass Spring Boot ilovasini ishga tushirish va konfiguratsiya qilish uchun
  * javobgar.
+ *
+ * @author SARDOR KUSHAKOV
+ * @version 1.0.0
+ * @see <a href="https://github.com/sardor-kushakov/file-upload-download">GitHub repository</a>
  */
 @SpringBootApplication
 public class FileUploadDownloadApplication {
-
     public static void main(String[] args) {
-
-        // GitHub: https://github.com/sardor-kushakov
-
         // Spring Boot ilovasini ishga tushirish
         SpringApplication.run(FileUploadDownloadApplication.class, args);
     }
-
 }
